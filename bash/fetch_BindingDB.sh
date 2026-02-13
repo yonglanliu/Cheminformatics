79e406a4ba5fb045ecf74e0f4f2eb5bc2a72eb0c
Yonglan Liu <122495576+yonglanliu@users.noreply.github.com>
2026-02-13 18:25:19 -0500

    Add fetch_BindingDB.sh script for data retrieval

---
+++ bash/fetch_BindingDB.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# ----------------------------
+# Load conda
+# ----------------------------
+source ~/bin/myconda
+conda activate chem
+
+echo "Conda environment activated: $(which python)"
+
+# ----------------------------
+# Variables
+# ----------------------------
+TARGET_CHEMBL_ID="Q08493"
+
+FINAL_OUT_PATH="smiles_plus_pIC50_BindingDB.csv"
+LOG_PATH="BindingDB_pipeline.log"
+
+RUN_SCRIPT="./scripts/fetch_BindingDB_data.py"
+
+# ----------------------------
+# Run pipeline
+# ----------------------------
+python "$RUN_SCRIPT" \
+    --uniprot "$TARGET_CHEMBL_ID" \
+    --out_csv "$FINAL_OUT_PATH" \
+    --log_path "$LOG_PATH"
+
+echo "Pipeline finished successfully."
